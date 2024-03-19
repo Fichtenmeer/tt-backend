8490c7e9ea539e69d6687b2bbd69458e6c1865ef
Alexander Eichenseer <alexander.eichenseer@cloudflight.io>
2024-03-19 19:58:44 +0100

    Init Spring App

---
+++ src/main/kotlin/de/kebonara/kronwinkl/TtKronwinklBackendApplication.kt
@@ -0,0 +1,11 @@
+package de.kebonara.kronwinkl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TtKronwinklBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<TtKronwinklBackendApplication>(*args)
+}
